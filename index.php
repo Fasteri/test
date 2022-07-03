83f578573c9b83cf0ed6fc53dfebf8d32b13348e
FastBro <draykin1701@gmail.com>
2022-07-04 00:10:28 +0300

    6.7.5

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-//   $say = htmlspecialchars($_POST['first_name']);
-//   $to  = htmlspecialchars($_POST['myForm']);
 
-//   echo  $say, ' ', $to;
-
-print_r($_POST);
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
