563fb4576cb143f722128da4e4f2cf04e4488195
FastBro <draykin1701@gmail.com>
2022-07-05 16:15:32 +0300

    8.0

--- index.php
+++ index.php
@@ -23,61 +23,66 @@
 	<div class="contact_wrapper">
 		<h1>Форма обратной связи</h1>
 		<form action="user.php" method="post" class="contact_form"  id="ok" name="myForm" >
-			
-			<p>Имя</p>
-			<div class="item">			
+		<p>Имя</p>
+			<div class="item" id="first">			
 				<input type="text" name="first_name" placeholder="">
-				<span class="error"></span>
+				<span class="error">Некорректное имя</span>
+				<span class="request">Укажите имя</span>
 			</div>
 			
 			<p>Фамилия</p>
-			<div class="item">				
+			<div class="item " id="second">				
 				<input type="text" name="last_name" placeholder="">
-				<span class="error"></span>
+				<span class="error">Некорректная фамилия</span>
+				<span class="request">Укажите  фамилию</span>
 			</div>
 
 			<p>Email</p>
-			<div class="item">			
+			<div class="item" id="third">			
 				<input id ="111" type="email" name="email" placeholder="">
-				<span class="error"></span>
+				<span class="error">Некорректный Е-майл</span>
+				<span class="request">Укажите Е-майл</span>
 			</div>
 
 			<p>Дата рождения</p>
-			<div class="item">			
-				<input type="text" name="age" placeholder="" id="age" maxlength="10">
-				<span class="error"></span>
+			<div class="item" id="fourth">			
+				<input type="text" name="age" placeholder="" id="age"  maxlength="10">
+				<span class="error">Некорректная дата<br>Укажите дату в формате дд.мм.гггг</span>
+				<span class="request">Укажите дату в формате дд.мм.гггг</span>
 			</div>
 
 			<p>Номер телефона</p>
-			<div class="item">			
+			<div class="item" id="fifth">			
 				<input type="text" name="tel" placeholder="+7 (___)___-__-__"  class="tel">
-				<span class="error"></span>
+				<span class="error">Некорректный номер телефона</span>
+				<span class="request">Укажите телефон</span>
 			</div>
 
 			<p>Город</p>
-			<div class="item">			
+			<div class="item" id="sixth">			
 				<input type="text" name="city" placeholder="">
-				<span class="error"></span>
+				<span class="error">Некорректное название города</span>
+				<span class="request">Укажите город</span>
 			</div>
 
 			<p>Портфолио</p>
-			<div class="item">			
+			<div class="item" id="seventh">			
 				<label for="file" class="feedback__label"  id="filee">
 					<span class="feedback__text">Выберите файл</span>
-					<input type="file"  id="file" name="document" class="feedback__file">
+					<span class="request">Выберите файл</span>
+					<input type="file"  id="file" name="document" class="feedback__file input_error">
 					<div class="feedback__file_name" id="filename"></div>
 				</label>
-				<span class="error"></span>
+				<span class="error">Выберите файл портофолио</span>
 			</div>
 
 			<input type="submit" id="999" value="Отправить заявку" class="feedback__button">
 		</form>
-		<!-- <iframe id="id_iframe" name="nm_iframe" style="display:none;"></iframe> -->
+
 	</div>
 		
 </div>
 <script src="js/main.js"></script>
-
 
 
 </body>
--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
-    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
-    var keyCode;
-    function mask(event) {
-        event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
-        if (pos < 3) event.preventDefault();
-        var matrix = "+7 (___) ___ ____",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
-            new_value = matrix.replace(/[_\d]/g, function(a) {
-                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-            });
-        i = new_value.indexOf("_");
-        if (i != -1) {
-            i < 5 && (i = 3);
-            new_value = new_value.slice(0, i)
-        }
-        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-            function(a) {
-                return "\\d{1," + a.length + "}"
-            }).replace(/[+()]/g, "\\$&");
-        reg = new RegExp("^" + reg + "$");
-        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-        if (event.type == "blur" && this.value.length < 5)  this.value = ""
-    }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false)
-
-  });
-
-});
 window.addEventListener("DOMContentLoaded", function() {
     [].forEach.call( document.querySelectorAll('.tel'), function(input) {
     var keyCode;
@@ -69,34 +34,36 @@ window.addEventListener("DOMContentLoaded", function() {
 
   });
   
-  var input = document.getElementById('age');
+
+  
+  // var input = document.getElementById('age');
  
-  var dateInputMask = function dateInputMask(elm) {
+  // var dateInputMask = function dateInputMask(elm) {
    
-      elm.addEventListener('keyup', function(e) {
-      if( e.keyCode < 47 || e.keyCode > 57) {
-        e.preventDefault();
-      }
+  //     elm.addEventListener('keyup', function(e) {
+  //     if( e.keyCode < 47 || e.keyCode > 57) {
+  //       e.preventDefault();
+  //     }
    
-     var len = elm.value.length;
+  //    var len = elm.value.length;
    
-      if(len !== 1 || len !== 3) {
-        if(e.keyCode == 47) {
-          e.preventDefault();
-        }
-      }
-     if(len === 2) {
-      if (e.keyCode !== 8 && e.keyCode !== 46) { 
-        elm.value = elm.value+'.';
-      }}
+  //     if(len !== 1 || len !== 3) {
+  //       if(e.keyCode == 47) {
+  //         e.preventDefault();
+  //       }
+  //     }
+  //    if(len === 2) {
+  //     if (e.keyCode !== 8 && e.keyCode !== 46) { 
+  //       elm.value = elm.value+'.';
+  //     }}
    
-  if(len === 5) {
-      if (e.keyCode !== 8 && e.keyCode !== 46) { 
-        elm.value = elm.value+'.';
-      }}
-    });
-  };
-  dateInputMask(input);
+  // if(len === 5) {
+  //     if (e.keyCode !== 8 && e.keyCode !== 46) { 
+  //       elm.value = elm.value+'.';
+  //     }}
+  //   });
+  // };
+  // dateInputMask(input);
 });
 
 
@@ -120,142 +87,174 @@ if (typeof el.innerText !== 'undefined') {
 
 
 document.getElementById('999').onclick = function (evt) {
-
   evt.preventDefault();
-  validate();
-
+  reset();
+  check();
 };
 
+ function reset(){
+  point=0;
+  total=0;
+  kok=0;
+ }
 
-function validate() {
-  let z = document.forms['myForm']["first_name"].value;
-if (z == "") {
-  alert("Укажите ваше имя");
-  return false;
-}
-if ( /^[a-zA-Zа-яА-ЯёЁІіЇїЄєҐґ']+$/.test(z)) {}
-else{
- alert ("Некорректное имя");
- return false;
-}
+var sum=0;
+var point =0;
+var total=0;
+var kok = 0;
+function validate(i){
+  
+  let a = document.forms['myForm'][i].value;
+  let b = document.querySelectorAll('.item')[i];
+  let d = document.querySelectorAll('.error')[i];
+  switch (i) {
+    case 0:
+    case 1:
+    case 5:
+      if (sum == 0){
+     
+        if ( /^[a-zA-Zа-яА-ЯёЁІіЇїЄєҐґ']+$/.test(a)){
+          d.style.display = d.style.display = 'none';
+          b.classList.remove('input_error');
+        } else{
+          point++;
+          kok++;
+          d.style.display = d.style.display = 'block';
+          b.classList.add('input_error');
+        }
+        if (point == 0) {
+          b.classList.add('green');
+        }
+        else{
+          b.classList.remove('green');
+        }
+      }
+  
+      break;
 
+    case 2:   
+      if (sum == 0){
+      function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+      if(validateEmail(a)) {
+        d.style.display = d.style.display = 'none';
+        b.classList.remove('input_error');
+      }
+        else{
+          kok++;
+          point++;
+          let c = document.getElementById('third');
+          let d = c.querySelector('.error');
+          d.style.display = d.style.display = 'block';
+          document.getElementById('third').classList.add('input_error');
+        }
+        if (point == 0) {
+          b.classList.add('green');
+        }
+        else{
+          b.classList.remove('green');
+        }
+      }
+      break;
+    
+    case 3:
+      if (sum == 0){
+          if (/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/.test(a)) {
+            d.style.display = d.style.display = 'none';
+            b.classList.remove('input_error');     
+          }
+          else {
+            kok++;
+            point++;
+            d.style.display = d.style.display = 'block';
+            b.classList.add('input_error');
+          }
 
-
-var b = document.forms["myForm"]["last_name"].value;
-if (b == "") {
-  alert("Укажите вашу фамилию");
-  return false;
-}
-if ( /^[a-zA-Zа-яА-ЯёЁІіЇїЄєҐґ']+$/.test(b)) {}
-   else{
-    alert ("Некорректная фамилия");
-    return false;
+    if (point == 0) {
+    b.classList.add('green');
+    }
+    else{
+    b.classList.remove('green');
+     }
   }
+      break;
+ 
+    case 4:
+      if (sum == 0){
+      let ee = a.length;
 
+      if (ee == 17){
+        d.style.display = d.style.display = 'none';
+        b.classList.remove('input_error');
+      }
+      else {
+        kok++;
+        point++;
+        d.style.display = d.style.display = 'block';
+        b.classList.add('input_error');
+      }
+      if (point == 0) {
+        b.classList.add('green');
+      }
+      else{
+        b.classList.remove('green');
+      }
+    }
+    break;
 
-
-var с = document.forms["myForm"]["email"].value;
-var inputEmail = document.getElementById("111");
-var emailVal = inputEmail.value;
-if (с == "") {
-  alert("Укажите ваш Е-майл");
-  return false;
-}
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-if(!validateEmail(emailVal)) {
-  console.log('email not valid');
-  alert("Неверный Е-майл");
-  return false;
-}
-
-
-
-var d = document.forms["myForm"]["age"].value;
-if (d == "") {
-  alert("Укажите вашу дату рождения");
-  return false;
-}
-if ( /^[0-9.]+$/.test(d)) {}
-   else{
-    alert ("Некорректная дата");
-    return false;
+    case 6:
+      if (sum == 0){
+        b.classList.add('green');
+      }
+      else{
+        b.classList.remove('green');
+      }
+    break;
   }
-
-
-
-var e = document.forms["myForm"]["tel"].value;
-if (e == "") {
-  alert("Укажите ваш телефон");
-  return false;
-}
-
-var ee = document.forms["myForm"]["tel"].value.length;
-if (ee == 17){
-}
-else {
-  alert("Неверный номер телефона");
-  return false;
-}
-
-
-
-var f = document.forms["myForm"]["city"].value;
-if (f == "") {
-  alert("Укажите ваш город");
-  return false;
-}
-if ( /^[a-zA-Zа-яА-ЯёЁІіЇїЄєҐґ']+$/.test(f)) {}
-   else{
-    alert ("Некорректное название города");
-    return false;
+  if (kok!==0){
+    point=1;
   }
-
-
-
-
-var g = document.forms["myForm"]["document"].value;
-if (g== "") {
-  alert("Выберите файл портофолио");
-  return false;
 }
 
-var bg = document.getElementById('bg');
-bg.classList.add('bg');
+function check(){
+  for (let i = 0; i < document.querySelectorAll('.item').length; i++) {    
+    let a = document.forms['myForm'][i].value;
+    let b = document.querySelectorAll('.item')[i];
+    let c = document.querySelectorAll('.request')[i];
+    let d = document.querySelectorAll('.error')[i];
+    if (a == ""){
+      sum++;
+      total++;
+      c.style.display = c.style.display = 'block';
+      b.classList.add('input_error');
+      d.style.display = d.style.display = 'none';
+    }
+    else{
+      c.style.display = c.style.display = 'none';
+      b.classList.remove('input_error'); 
+    }
 
-var aler = document.getElementById("322");
-aler.style.display = aler.style.display = 'block';
+    validate(i);
+    sum=0;
+    point=0;
+  }
+  if ((total==0)&(kok==0)){
 
+    var bg = document.getElementById('bg');
+    bg.classList.add('bg');
 
-
-
-
+    var aler = document.getElementById("322");
+    aler.style.display = aler.style.display = 'block';
+  }
+  
+}
 
 
 var finish = document.getElementById('777');
 finish.addEventListener('click', function(){
   document.forms['myForm'].submit();
-  // document.getElementById('ok').target = "dasd" ;
-  // document.getElementById('id_iframe').remove();
-  // document.forms['myForm'].submit();
-  // window.location.reload(true);
+
   
-  
-  
-
-
-
-// // var finishOk = document.getElementById('ok');
-
 });
-
-
-}
-
-
-
-
-
-// })
